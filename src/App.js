55e87b5f5eeadc9d74c4bffc976fb31ea2a10288
Akshara Uke <akshara.uke@gmail.com>
2022-03-07 21:19:55 +0530

    added state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import {React, useState} from 'react';
 import logo from './logo.svg';
 import './components/CenterPane.css';
 import LeftMenu from './components/LeftMenu';
 import CenterPane from './components/CenterPane';
 function App() {
+
+  const [data,setCentrePaneData] = useState('');
+
+  function submitMenuChange(itemClicked) {
+    let moreInfo = "Some more stuff about...." + itemClicked;
+    setCentrePaneData(moreInfo);
+  };
   return (
     <div id="container">
-    <CenterPane/>
-    <LeftMenu/>
+    <CenterPane data={data}/>
+    <LeftMenu onMenuSelect={submitMenuChange}/>
 
    </div>
   );
--- src/components/LeftMenu.js
+++ src/components/LeftMenu.js
@@ -1,29 +1,39 @@
-import './LeftMenu.css';
+import "./LeftMenu.css";
 
-const LeftMenu = () =>  {
-    return (
-		<div id="left" class="column">
-            <div class="dropdown">
-  <button class="dropbtn">Nodes</button>
-  <div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div>
-<div class="dropdown">
-  <button class="dropbtn">Services</button>
-  <div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div>
-			</div>
-        
-	
-
-    );
+const LeftMenu = (props) => {
+  function sumitMenuClick(event) {
+    props.onMenuSelect(event.currentTarget.firstChild.data);
+  }
+  return (
+    <div id="left" class="column">
+      <div class="dropdown">
+        <button class="dropbtn">Nodes</button>
+        <div class="dropdown-content">
+          <a href="#" onClick={sumitMenuClick}>
+            Link 1
+          </a>
+          <a href="#" onClick={sumitMenuClick}>
+            Link 2
+          </a>
+          <a href="#" onClick={sumitMenuClick}>
+            Link 3
+          </a>
+        </div>
+      </div>
+      <div class="dropdown">
+        <button class="dropbtn">Services</button>
+        <div class="dropdown-content">
+          <a href="#" onClick={sumitMenuClick}>
+            Link a
+          </a>
+          <a href="#" onClick={sumitMenuClick}>
+            Link b
+          </a>
+          <a href="# onClick={sumitMenuClick}">Link c</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftMenu;
